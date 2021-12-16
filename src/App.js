d3ef611516e004efc20fd804afeb9af60ff5e730
kumikoyamagata <sun.kumiko@protonmail.com>
2021-12-16 01:16:50 -0600

    add conditional render to the Gallery

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 //App.js is the center of the application (root compornent)//
 
-import React from 'react';
+import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
+import Gallery from './components/Gallery';
 /*
 function App() {
   return (
@@ -24,17 +25,46 @@ function App() {
     </div>
   );
 }
-*/
+====
 
 function App() {
+
   return (
     <div>
-      <Nav />
+      <Nav/>
       <main>
-        <About />
+        <Gallery/>
+        <About/>
       </main>
     </div>
   );
 }
+*/
+function App() {
+  const [categories] = useState([
+    {
+      name: 'commercial',
+      description: 'Photos of grocery stores, food trucks, and other commercial projects',
+    },
+    { name: 'portraits', description: 'Portraits of people in my life' },
+    { name: 'food', description: 'Delicious delicacies' },
+    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
+  ]);
 
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
+  return (
+    <div>
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+      ></Nav>
+      <main>
+        <Gallery currentCategory={currentCategory}></Gallery>
+        <About></About>
+      </main>
+    </div>
+  );
+}
 export default App;
